aa8864589efe8a3acb2f72587b633006ddacf632
sam <samignesh1998@gmail.com>
2020-04-24 15:33:25 +0530

    grocerysos

--- src/app/components/fruits/fruits.component.ts
+++ src/app/components/fruits/fruits.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import {NgModule} from '@angular/core'
+import {fruites} from '../../../searchfiles/fruites.model';
 
 @Component({
   selector: 'app-fruits',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fruits.component.css']
 })
 export class FruitsComponent implements OnInit {
-
+   fruites:fruites[]=[];
+   searchText:string;
   constructor() { }
 
   ngOnInit(): void {
+   /* this.fruites=[
+      {
+        "name":"apple",
+      }
+    ];*/
+    
+  }
+
+  Search()
+  {
+    if(this.searchText!="")
+    {
+          
+    this.fruites=this.fruites.filter(res=>{
+      return res.name.toLocaleLowerCase().match(this.searchText.toLocaleLowerCase());
+    });
+    }
+    else if(this.searchText == "")
+    {
+      this.ngOnInit();
+    }
   }
 
 }
